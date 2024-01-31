343abc8bd9928f351e14c5a34a9e9760cd8820ff
alibayomy <alibayomy12@gmail.com>
2024-01-31 21:44:53 +0200

    Modifying the main script by changing the database folder name

--- main.sh
+++ main.sh
@@ -1,13 +1,17 @@
 #!/usr/bin/bash
 #This is the main script of my bash project
-select command in "CREATE DB" "LIST DB" "DROP DB" "CONNECT TO DB" "exit"
+select command in "CREATE DataBases" "LIST DataBases" "DROP DataBases" "CONNECT TO DataBases" "exit"
 do
 	case $REPLY in
 		1) echo "Creating database"
 			source ./scripts/1-creating_db_script
 			;;
 		2) echo "-------------------Listing Database-----------------------"
-			ls ./DB
+			if [[ -d ./DataBases ]];then
+				ls ./DataBases
+			else
+				echo "There is No databases created Press 1 to create the first :) :) :)"
+			fi
 			;;
 		3) echo "Dropping a database"
 			source ./scripts/2-deleting_db_script
