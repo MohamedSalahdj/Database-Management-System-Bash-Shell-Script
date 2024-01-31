d5dd2d5f39b4f9be90c1e310c9cf88673377ef9c
MohamedSalahdj <mohamedsamydj@gmail.com>
2024-01-31 23:09:20 +0200

    modified directory name, handle some error 'main.sh'

--- main.sh
+++ main.sh
@@ -1,33 +1,5 @@
 #!/usr/bin/bash
 #This is the main script of my bash project
-<<<<<<< HEAD
-select command in "CREATE DataBases" "LIST DataBases" "DROP DataBases" "CONNECT TO DataBases" "exit"
-do
-	case $REPLY in
-		1) echo "Creating database"
-			source ./scripts/1-creating_db_script
-			;;
-		2) echo "-------------------Listing Database-----------------------"
-			if [[ -d ./DataBases ]];then
-				ls ./DataBases
-			else
-				echo "There is No databases created Press 1 to create the first :) :) :)"
-			fi
-			;;
-		3) echo "Dropping a database"
-			source ./scripts/2-deleting_db_script
-			;;
-		4) echo "Connect to database"
-			 ./scripts/table_menu.sh
-			;;
-		5) echo "Bye"
-			exit
-			;;
-		*) echo "Command not found"
-			;;
-	esac
-=======
-
 while true;
 	do
 	echo
@@ -36,7 +8,7 @@ while true;
 		
 		case $REPLY in
 			1) echo -e "Creating database"
-				source ./scripts/1-creating_db_script
+				source ./Scripts/1-creating_db_script
 				;;
 			2) echo -e "------------------- Listing Database -----------------------\n"
 				if [[ -d ./DataBases ]];then
@@ -47,10 +19,10 @@ while true;
 				fi
 				;;
 			3) echo -e "Dropping a database"
-				source ./scripts/2-deleting_db_script
+				source ./Scripts/2-deleting_db_script
 				;;
 			4) echo -e "Connect to database"
-				 ./scripts/table_menu.sh
+				 ./Scripts/table_menu.sh
 				;;
 			5) echo  "Bye"
 				exit
@@ -60,5 +32,4 @@ while true;
 		esac
 		break
 	done
->>>>>>> d6fa45b (modified in main.sh to handle main manue of project)
 done
