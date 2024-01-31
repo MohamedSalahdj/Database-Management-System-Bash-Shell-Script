16a9dfdd50b6e1c2b86cea3b72b8f044fdaf41b2
MohamedSalahdj <mohamedsamydj@gmail.com>
2024-01-31 22:07:19 +0200

    modified in main.sh to handle main manue of project

--- main.sh
+++ main.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/bash
 #This is the main script of my bash project
+<<<<<<< HEAD
 select command in "CREATE DataBases" "LIST DataBases" "DROP DataBases" "CONNECT TO DataBases" "exit"
 do
 	case $REPLY in
@@ -25,4 +26,39 @@ do
 		*) echo "Command not found"
 			;;
 	esac
+=======
+
+while true;
+	do
+	echo
+	select command in "CREATE DataBases" "LIST DataBases" "DROP DataBases" "CONNECT TO DataBases" "exit"
+	do
+		
+		case $REPLY in
+			1) echo -e "Creating database"
+				source ./scripts/1-creating_db_script
+				;;
+			2) echo -e "------------------- Listing Database -----------------------\n"
+				if [[ -d ./DataBases ]];then
+					ls ./DataBases
+					echo -e "-------------------------------------------------------\n"
+				else
+					echo -e "There is No databases created Press 1 to create the first :) :) :)\n"
+				fi
+				;;
+			3) echo -e "Dropping a database"
+				source ./scripts/2-deleting_db_script
+				;;
+			4) echo -e "Connect to database"
+				 ./scripts/table_menu.sh
+				;;
+			5) echo  "Bye"
+				exit
+				;;
+			*) echo -e "Command not found"
+				;;
+		esac
+		break
+	done
+>>>>>>> d6fa45b (modified in main.sh to handle main manue of project)
 done
